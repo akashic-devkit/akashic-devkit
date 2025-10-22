627ab45e65ed18e28f79e8bfcf14044b8b483aa9
Jinwon Jung <wlsdnjs16521@gmail.com>
2025-10-22 23:21:16 +0900

    chore(root): enable @commitlint/format

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,7 @@
 // commitlint.config.js
 export default {
   extends: ["@commitlint/config-conventional"],
+  formatter: "@commitlint/format",
   rules: {
     "scope-empty": [2, "never"],
     "scope-enum": [2, "always", ["web", "cli", "config", "root"]],
