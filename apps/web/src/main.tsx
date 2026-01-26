07bdc34ae20a1b42b2609adeeba5d63f19f446c8
John <john10.kim@cheilpengtai.com>
2026-01-26 17:35:16 +0900

    feat(web): tanstack query 설치, provider 세팅

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -5,6 +5,7 @@ import "./index.css";
 import { routeTree } from "./routeTree.gen";
 import NotFound from "./components/layout/NotFound";
 import { ThemeProvider } from "./components/theme/ThemeProvider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const router = createRouter({
   routeTree,
@@ -17,10 +18,14 @@ declare module "@tanstack/react-router" {
   }
 }
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </ThemeProvider>
   </StrictMode>
 );
